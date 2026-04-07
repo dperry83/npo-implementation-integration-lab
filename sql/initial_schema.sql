2c556687f387a7ca58e1fb409a8e9c630ea56976
David Perry <125750519+dperry83@users.noreply.github.com>
2026-04-07 13:50:04 -0400

    Update initial_schema.sql

--- sql/initial_schema.sql
+++ sql/initial_schema.sql
@@ -1,14 +1,14 @@
 CREATE TABLE constituents (
-    id          SERIAL NOT NULL PRIMARY KEY UNIQUE,
-    name        VARCHAR,
-    email       VARCHAR,
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR NOT NULL,
+    email       VARCHAR UNIQUE,
     phone       VARCHAR,
     address     VARCHAR,
-    create_at   DATE NOT NULL
+    created_at   DATE NOT NULL
 );
 
 CREATE TABLE attributes (
-    id          SERIAL NOT NULL PRIMARY KEY UNIQUE,
+    id          SERIAL NOT NULL PRIMARY KEY,
     type        VARCHAR NOT NULL,
     name        VARCHAR NOT NULL,
     description VARCHAR
@@ -16,36 +16,42 @@ CREATE TABLE attributes (
 
 CREATE TABLE constituents_attributes (
     constituent_id  INT NOT NULL REFERENCES constituents(id),
-    attribute_id    INT NOT NULL REFERENCES attributes(id)
+    attribute_id    INT NOT NULL REFERENCES attributes(id),
+    PRIMARY KEY (constituent_id, attribute_id)
 );
 
 CREATE TABLE donations (
-    id          SERIAL NOT NULL PRIMARY KEY UNIQUE,
-    amount      DECIMAL NOT NULL
-    date        DATE NOT NULL
+    id              SERIAL PRIMARY KEY,
+    constituent_id  INT NOT NULL REFERENCES constituents(id),
+    campaign_id     INT REFERENCES campaigns(id),
+    amount          DECIMAL NOT NULL,
+    donated_at      DATE NOT NULL
 );
 
 CREATE TABLE campaigns (
-    id          SERIAL NOT NULL PRIMARY KEY UNIQUE,
+    id          SERIAL PRIMARY KEY,
     name        VARCHAR NOT NULL UNIQUE,
     goal        DECIMAL,
     start_at    DATE NOT NULL,
     end_at      DATE
 );
 
-CREATE TABLE donations_campaigns (
-    donation_id     INT NOT NULL REFERENCES donations(id),
-    campaign_id     INT NOT NULL REFERENCES campaigns(id)
-);
+-- CREATE TABLE donations_campaigns (
+--     donation_id     INT NOT NULL REFERENCES donations(id),
+--     campaign_id     INT NOT NULL REFERENCES campaigns(id),
+--     PRIMARY KEY (donation_id, campaign_id)
+-- );
 
 CREATE TABLE interactions (
-    id          SERIAL NOT NULL PRIMARY KEY UNIQUE,
-    channel     VARCHAR NOT NULL,
-    date        DATE NOT NULL,
-    content     VARCHAR 
+    id              SERIAL PRIMARY KEY,
+    constituent_id  INT NOT NULL REFERENCES constituents(id),
+    channel         VARCHAR NOT NULL,
+    happened_on     DATE NOT NULL,
+    content         VARCHAR 
 );
 
-CREATE TABLE constituents_interactions (
-    constituent_id  INT NOT NULL REFERENCES constituents(id),
-    interaction_id  INT NOT NULL REFERENCES interactions(id)
-);
+-- CREATE TABLE constituents_interactions (
+--     constituent_id  INT NOT NULL REFERENCES constituents(id),
+--     interaction_id  INT NOT NULL REFERENCES interactions(id),
+--     PRIMARY KEY (constituent_id, interaction_id)
+-- );
